8922cfa85369b6b58e90584ccd43b0339468bfba
duonghuu <60811325+duonghuu@users.noreply.github.com>
2020-04-07 20:53:35 +0700

    hm edit header

--- templates/layout/header.php
+++ templates/layout/header.php
@@ -18,8 +18,11 @@
               <input type="text" class="form-control keyword" required="true" onkeypress="doEnter(event)" value="<?=_nhaptukhoatimkiem?>..." onclick="if(this.value=='<?=_nhaptukhoatimkiem?>...'){this.value=''}" onblur="if(this.value==''){this.value='<?=_nhaptukhoatimkiem?>...'}"> <span onclick="onSearch($(this));return false;" class="btn_search">Tìm</span> </div>
             </div>
           <?php } ?>
+          <div class="hd-right-box">
+            
             <div class="hd-hotline"><img src="images/hd-hotline.jpg" alt=""><a href="tel:<?=preg_replace('/[^0-9]/','',$company['dienthoai']);?>" ><?= $company['dienthoai'] ?></a></div>
             <a href="gio-hang.html" class="giohang_fix" ><img src="images/cart.jpg" alt="cart"><span><?= count($_SESSION["cart"]) ?></span></a> 
+          </div>
           </div>
         </div>
       </div>
